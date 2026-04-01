3950cc0c9ffe460d37aeed2f3a42d1fa5f834002
Aevan-354 <njugunaevans801@gmail.com>
2026-04-01 09:55:26 +0300

    upadate sample file

--- sample.py
+++ sample.py
@@ -1,8 +1,39 @@
+#integer example
 age=30
 print(type(age))
 
+#float example
 mass=99.9
 print(type(mass))
 
+#complex Number example
 complex_number = 2+3j
 print(type(complex_number))
+
+#string example
+name="Alice"
+print(type(name))
+
+#Boolen Example
+is_active= True
+print(type(is_active))
+
+#example of a list
+numbers=[3,5,4]
+print(type(numbers))
+
+#example of tuple
+coordinates=(10,20)
+print(type(coordinates))
+
+#example of a range
+numbers=range(5)
+print(numbers)
+print(type(numbers))
+
+#example of a dictionary
+student={"name":"Aevan", "age":25, "major":"Data Engineer"}
+print(student)
+print(type(student))
+
+print("git is working")
